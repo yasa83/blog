58af6e824f01d87f1deee2787a327f9ca89f90c2
Your Name <you@example.com>
2018-11-28 20:05:50 +0900

    Create-new.html.erb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,16 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:title, :text)
+  end
 end
