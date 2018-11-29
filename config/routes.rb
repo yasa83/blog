2d54d85e07351eae2e573bc92ee72f0c05d02cc8
Your Name <you@example.com>
2018-11-29 09:00:10 +0900

    発展学習1のdeviseまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root                    'tweets#index'
   get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
   get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_013513) do
+ActiveRecord::Schema.define(version: 2018_11_28_235810) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
     t.text "text"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
-    t.string "title"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
