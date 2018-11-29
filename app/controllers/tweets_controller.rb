a4af1386eabd9609a558c1ff8ed8f918bf0ac884
Your Name <you@example.com>
2018-11-29 10:02:27 +0900

    unless user_signed_in, user can't post

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
   def index
     @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -15,5 +16,9 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:title, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
