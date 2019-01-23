cddb62c1dfeaaad77f3a18098c24b91b2c03a33c
Your Name <you@example.com>
2019-01-23 13:47:10 +0900

    comiit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,16 +2,29 @@ class TweetsController < ApplicationController
 
   before_action :move_to_index, except: :index
   def index
-    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(title: tweet_params[:title], text: tweet_params[:text])
+    Tweet.create(title: tweet_params[:title], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    tweet =Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params) if tweet.user_id == current_user.id
+  end
 
   private
   def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
   get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  delete 'tweets/:id'     =>  'tweets#destroy'
+  get   'tweets/:id/edit' =>  'tweets#edit'
+  patch 'tweets/:id'     =>   'tweets#update'
+  get   'users/:id'   =>  'users#show'
+
 end
